4703bb897450da4e8b14ab461abe70ed9a694be1
FacuM <facumo.fm@gmail.com>
2024-05-19 23:02:50 -0300

    Fix read on `undefined` if `animateOnDataChange` is `true`

--- src/LineChart/index.ts
+++ src/LineChart/index.ts
@@ -462,6 +462,8 @@ export const useLineChart = (props: extendedLineChartPropsType) => {
   if (animateOnDataChange && animations) {
     animations.forEach((item, index) => {
       item.addListener((val) => {
+        if (typeof data[index] === 'undefined') { return; }
+
         const temp = data[index]?.value ?? 0
         data[index].value = val.value
         let pp = ''
