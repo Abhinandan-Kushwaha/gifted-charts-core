599ada733e5b6749fd05393611b6d34b15617ceb
Sadat Taseen <67076071+sttaseen@users.noreply.github.com>
2025-06-09 20:49:15 +1200

    feat: add props for spread in line chart

--- src/LineChart/types.ts
+++ src/LineChart/types.ts
@@ -564,6 +564,10 @@ export interface LineChartBicolorPropsType {
   verticalLinesSpacing?: number
   hideAxesAndRules?: boolean
   areaChart?: boolean
+  
+  spreadAreaData?: { lower: number; upper: number }[];
+  spreadAreaColor?: string;
+  spreadAreaOpacity?: number;
 
   disableScroll?: boolean
   showScrollIndicator?: boolean
